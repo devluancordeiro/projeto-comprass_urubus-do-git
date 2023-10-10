6744b468f605c79dcff959f5e97b5eb57e541ff1
Luiz Carlos Rodrigues Alves Neto <lucarodriguesalves@gmail.com>
2023-10-10 14:08:04 -0300

    merge: Completed-merge!

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {View} from 'react-native';
 import Home from './screens/Home';
 
 function App(): JSX.Element {
-  const [text1, setText1] = useState('');
-  const [text2, setText2] = useState('');
-
   return (
     <View>
       <Home />
