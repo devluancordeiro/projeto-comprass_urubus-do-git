f71f971dee9fd7f42a6fd8c6b776a62e43467c15
Felipe Silva <Taxistaloko14@outlook.com>
2023-10-10 10:33:54 -0300

    feat: add base home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {View} from 'react-native';
-import RedButton from './components/ui/RedButton';
 import Home from './screens/Home';
 
 function App(): JSX.Element {
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,104 @@
 import React from 'react';
-import {View} from 'react-native';
-import RedButton from '../components/ui/RedButton';
+import {
+  View,
+  ImageBackground,
+  StyleSheet,
+  Image,
+  Text,
+  StatusBar,
+} from 'react-native';
+import Products from '../components/api/Products';
 
 const Home = () => {
-  return <RedButton onPress={function (): void {}}>LOGIN</RedButton>;
+  return (
+    <>
+      <StatusBar
+        backgroundColor={'transparent'}
+        translucent={true}
+        barStyle={'light-content'}
+      />
+      <View style={styles.view}>
+        <ImageBackground
+          source={require('../assets/images/background-compass.png')}
+          style={styles.background}>
+          <View style={styles.viewSearch}>
+            <Image
+              source={require('../assets/images/redDot.png')}
+              style={styles.redDot}
+            />
+            <Image
+              source={require('../assets/images/search.png')}
+              style={styles.search}
+            />
+          </View>
+          <Image
+            source={require('../assets/images/Comprass-logo1.png')}
+            style={styles.imageCompras}
+          />
+          <View style={styles.viewTextCart}>
+            <Text style={styles.text}>Aqui você sempre ganha!</Text>
+            <Image
+              source={require('../assets/images/cart-svgrepo-com1.png')}
+              style={styles.imageCart}
+            />
+          </View>
+          <Products />
+        </ImageBackground>
+      </View>
+    </>
+  );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+  },
+  background: {
+    width: '100%',
+    height: 400,
+    marginBottom: 16,
+  },
+  imageCompras: {
+    alignSelf: 'center',
+    width: '70%',
+    height: 60,
+    marginTop: '41%',
+    marginBottom: '27%',
+    marginHorizontal: 56,
+  },
+  text: {
+    color: 'white',
+    fontSize: 18,
+    marginHorizontal: 16,
+    fontFamily: 'OpenSans-SemiBold',
+  },
+  imageCart: {
+    height: 46,
+    width: 46,
+  },
+  viewTextCart: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  viewSearch: {
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    top: 66,
+    right: 16,
+    position: 'absolute',
+  },
+  search: {
+    height: 26,
+    width: 26,
+    marginRight: 12,
+    marginBottom: 4,
+  },
+  redDot: {
+    position: 'absolute',
+    height: 46,
+    width: 46,
+  },
+});
