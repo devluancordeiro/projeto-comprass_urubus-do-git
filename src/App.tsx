c89c22ef6a9e764c3ca33135be2fb842195c6053
Luan Cordeiro <santos.luancordeiro@gmail.com>
2023-10-10 15:41:54 -0300

    fix: Hide navigation header

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const Tab = createBottomTabNavigator();
 function MyTabs() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={{headerShown: false}}>
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Cart" component={Cart} />
       </Tab.Navigator>
