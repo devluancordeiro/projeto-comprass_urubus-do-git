fe7cd0d588a9f06418cef4367dc0116438b110bd
Felipe Silva <Taxistaloko14@outlook.com>
2023-10-11 12:19:47 -0300

    feat: Profile screen basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {View} from 'react-native';
 import Home from './screens/Home';
+import Profile from './screens/Profile';
 
 function App(): JSX.Element {
   return (
     <View>
-      <Home />
+      <Profile />
     </View>
   );
 }
