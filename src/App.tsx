171f3a240a5e93a13562eabeb0a5a3b9fe3d4aac
Luiz Carlos Rodrigues Alves Neto <lucarodriguesalves@gmail.com>
2023-10-11 16:47:38 -0300

    Merge: Finished Merging

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import SingUp from './screens/SignUp';
-import {createStackNavigator} from '@react-navigation/stack';
-import Login from './screens/Login';
-import ForgotPassword from './screens/ForgotPassword';
-import {NavigationContainer} from '@react-navigation/native';
-import {Colors} from './constants/styles';
 import {StoreContextProvider} from './contexts/StoreContext';
 import StoreFlow from './routes/StoreFlow';
 
 function App(): JSX.Element {
-  const Stack = createStackNavigator();
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{cardStyle: {backgroundColor: Colors.black}}}>
-        <Stack.Screen name="signup" component={SingUp} />
-        <Stack.Screen name="login" component={Login} />
-        <Stack.Screen name="forgot" component={ForgotPassword} />
-      </Stack.Navigator>
-    </NavigationContainer>
     <StoreContextProvider>
       <StoreFlow />
     </StoreContextProvider>
--- src/routes/StoreFlow.tsx
+++ src/routes/StoreFlow.tsx
@@ -6,73 +6,90 @@ import Cart from '../screens/Cart';
 import Profile from '../screens/Profile';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {Colors} from '../constants/styles';
+import {createStackNavigator} from '@react-navigation/stack';
+import SingUp from '../screens/SignUp';
+import Login from '../screens/Login';
+import ForgotPassword from '../screens/ForgotPassword';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function MainFlow() {
+  return (
+    <Tab.Navigator screenOptions={{headerShown: false}}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({size, color, focused}) => {
+            if (focused) {
+              return (
+                <Ionicons
+                  name={'home-sharp'}
+                  size={size}
+                  color={Colors.red_500}
+                />
+              );
+            }
+            return <Ionicons name={'home-outline'} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: Colors.red_500,
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={Cart}
+        options={{
+          tabBarIcon: ({size, color, focused}) => {
+            if (focused) {
+              return (
+                <Ionicons name={'cart'} size={size} color={Colors.red_500} />
+              );
+            }
+            return <Ionicons name={'cart-outline'} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: Colors.red_500,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({size, color, focused}) => {
+            if (focused) {
+              return (
+                <Ionicons name={'person'} size={size} color={Colors.red_500} />
+              );
+            }
+            return (
+              <Ionicons name={'person-outline'} size={size} color={color} />
+            );
+          },
+          tabBarActiveTintColor: Colors.red_500,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+function AuthFlow() {
+  return (
+    <Stack.Navigator
+      screenOptions={{cardStyle: {backgroundColor: Colors.black}}}>
+      <Stack.Screen name="signup" component={SingUp} />
+      <Stack.Screen name="login" component={Login} />
+      <Stack.Screen name="forgot" component={ForgotPassword} />
+    </Stack.Navigator>
+  );
+}
 
 function StoreFlow() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{headerShown: false}}>
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{
-            tabBarIcon: ({size, color, focused}) => {
-              if (focused) {
-                return (
-                  <Ionicons
-                    name={'home-sharp'}
-                    size={size}
-                    color={Colors.red_500}
-                  />
-                );
-              }
-              return (
-                <Ionicons name={'home-outline'} size={size} color={color} />
-              );
-            },
-            tabBarActiveTintColor: Colors.red_500,
-          }}
-        />
-        <Tab.Screen
-          name="Cart"
-          component={Cart}
-          options={{
-            tabBarIcon: ({size, color, focused}) => {
-              if (focused) {
-                return (
-                  <Ionicons name={'cart'} size={size} color={Colors.red_500} />
-                );
-              }
-              return (
-                <Ionicons name={'cart-outline'} size={size} color={color} />
-              );
-            },
-            tabBarActiveTintColor: Colors.red_500,
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={Profile}
-          options={{
-            tabBarIcon: ({size, color, focused}) => {
-              if (focused) {
-                return (
-                  <Ionicons
-                    name={'person'}
-                    size={size}
-                    color={Colors.red_500}
-                  />
-                );
-              }
-              return (
-                <Ionicons name={'person-outline'} size={size} color={color} />
-              );
-            },
-            tabBarActiveTintColor: Colors.red_500,
-          }}
-        />
-      </Tab.Navigator>
+      <Stack.Navigator initialRouteName="app" headerMode="none">
+        <Stack.Screen name="app" component={MainFlow} />
+        <Stack.Screen name="auth" component={AuthFlow} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
