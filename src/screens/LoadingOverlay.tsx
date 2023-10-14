528e4c93c9de65f616033fdca5670af1ff1fa408
Luiz Carlos Rodrigues Alves Neto <lucarodriguesalves@gmail.com>
2023-10-14 20:33:30 -0300

    feat: added loading screen

--- src/screens/LoadingOverlay.tsx
+++ src/screens/LoadingOverlay.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {Colors} from '../constants/styles';
 
 interface LoadingOverlayProps {
   message: string;
@@ -7,26 +14,31 @@ interface LoadingOverlayProps {
 
 const LoadingOverlay: React.FC<LoadingOverlayProps> = ({message}) => {
   return (
-    <View style={styles.rootContainer}>
+    <ImageBackground
+      source={require('../../assets/images/auth-background.png')}
+      resizeMode="cover"
+      style={styles.backgroundImage}>
       <Text style={styles.message}>{message}</Text>
-      <ActivityIndicator size="large" />
-    </View>
+      <ActivityIndicator size="large" color={Colors.red_500} />
+    </ImageBackground>
   );
 };
 
 export default LoadingOverlay;
 
 const styles = StyleSheet.create({
-  rootContainer: {
+  backgroundImage: {
     flex: 1,
+    position: 'absolute',
+    width: '100%',
+    height: '70%',
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 32,
   },
   message: {
-    color: 'black',
+    color: Colors.white,
     fontSize: 16,
     marginBottom: 12,
-    fontFamily: 'RobotoFlex-Regular',
+    fontFamily: 'OpenSans-Regular',
   },
 });
