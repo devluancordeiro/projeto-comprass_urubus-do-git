4e557c03a1aa215b6de52ab6244c177574ad3219
Luiz Carlos Rodrigues Alves Neto <lucarodriguesalves@gmail.com>
2023-10-17 14:44:10 -0300

    fix: adjusted some stuff all arround

--- src/components/auth/AuthFormHandler.tsx
+++ src/components/auth/AuthFormHandler.tsx
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react';
 import AuthForm from './AuthForm';
 import {ImageBackground, StyleSheet, View} from 'react-native';
-import {AuthContext} from './AuthContext';
+import {AuthContext} from '../context/AuthContext';
 
 interface FormData {
   name?: string | undefined;
